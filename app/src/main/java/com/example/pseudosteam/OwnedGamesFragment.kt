5183519bbe167504f87d86e916b63f9537b5bb75
nayaantaki <nayaantaki@gmail.com>
2024-12-03 09:32:30 -0500

    Initial commit, 3 fragments added

---
+++ app/src/main/java/com/example/pseudosteam/OwnedGamesFragment.kt
@@ -0,0 +1,19 @@
+package com.example.pseudosteam
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class OwnedGamesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_owned_games, container, false)
+    }
+
+}
